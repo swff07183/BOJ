c2c0d9268bb8c196af1294063cf51305b89b1638
Hong Seong Mok <swff07183@gmail.com>
2022-07-21 02:48:45 +0900

    gaji

--- gaji_muchim/0720/B_1986_S2.py
+++ gaji_muchim/0720/B_1986_S2.py
@@ -1,3 +1,56 @@
 """
 체스
 """
+import sys
+sys.stdin = open('input.txt')
+input = lambda: sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+
+board = [[0 for _ in range(m+1)] for _ in range(n+1)]
+
+# arr = [[] for _ in range(3)]
+di = [-1, -1, -1, 0, 0, 1, 1, 1]
+dj = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+ki = [-2, -1, -2, -1, 2, 1, 2, 1]
+kj = [-1, -2, 1, 2, -1, -2, 1, 2]
+
+queen = list(map(int, input().split()))
+knight = list(map(int, input().split()))
+pawn = list(map(int, input().split()))
+for x in range(pawn[0]):
+    i, j = pawn[x*2+1], pawn[x*2+2]
+    board[i][j] = 1
+
+for x in range(knight[0]):
+    i, j = knight[x*2+1], knight[x*2+2]
+    board[i][j] = 2
+    for d in range(8):
+        ni = i + ki[d]
+        nj = j + kj[d]
+        if not (1<=ni<=n and 1<=nj<=m):
+            continue
+        if board[ni][nj] == 0:
+            board[ni][nj] = -1
+
+for x in range(queen[0]):
+    i, j = queen[x*2+1], queen[x*2+2]
+    board[i][j] = 3
+for x in range(queen[0]):
+    i, j = queen[x*2+1], queen[x*2+2]
+    for d in range(8):
+        for k in range(1, n*m):
+            ni = i + (di[d]*k)
+            nj = j + (dj[d]*k)
+            if not (1<=ni<=n and 1<=nj<=m) or board[ni][nj]>0:
+                break
+            if board[ni][nj] == 0:
+                board[ni][nj] = -1
+
+ans = 0
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if board[i][j]==0:
+            ans += 1
+print(ans)
--- gaji_muchim/0720/C_1421_S3.py
+++ gaji_muchim/0720/C_1421_S3.py
@@ -1,6 +1,26 @@
 """
 나무꾼 이다솜
+N <= 50
+나무 길이 <= 10,000
+500,000 -> 완탐 ㄱ
+
+처음에 틀림
+안파는게 좋은 경우도 있다..
 """
 N, C, W = map(int, input().split())
 arr = [int(input()) for _ in range(N)]
 
+max_len = max(arr)  # 최대 길이 저장
+
+ans = 0
+for l in range(1, max_len+1):       # 모든 경우의 수 보면서
+    total = 0                       # l로 잘랐을 때 벌 수 있는 돈
+    for i in range(N):              # 나무 하나씩 확인해서
+        if l > arr[i]:              # 못자르면 패쓰
+            continue
+        cut = arr[i]//l if arr[i]%l else arr[i]//l-1    # 자르는 횟수
+        total += max(0, W*l*(arr[i]//l) - cut*C)        # 가격 구해서 더해주기
+
+    ans = max(total, ans)
+    
+print(ans)
