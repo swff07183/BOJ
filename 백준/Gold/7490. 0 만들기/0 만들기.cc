396ed99f372f347097c66f04c9b5e4250a5003e1
SeongMok Hong <swff07183@gmail.com>
2023-01-30 23:06:50 +0900

    [Gold V] Title: 0 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.cc
+++ 백준/Gold/7490. 0 만들기/0 만들기.cc
@@ -10,7 +10,6 @@ int calc(string s);
 int to_int(char n);
 
 int T, N;
-int visited[10] = {0, };
 char oper[] = {' ', '+', '-'};
 vector<string> v;
 
@@ -21,9 +20,6 @@ int main() {
 
   cin >> T;
   for(int tc=1; tc<=T; tc++) {
-    // visited 초기화
-    for(int i=0; i<10; i++) visited[i] = 0;
-    // 
     cin >> N;
     backtracking(1, "1");
     v.push_back("");
@@ -35,7 +31,9 @@ int main() {
 }
 
 void backtracking(int cnt, string s) {
+  // 가능한 모든 식 구하기
   if (cnt == N) {
+    // 계산해주고 0이되면 vector에 추가
     if(calc(s) == 0) {
       v.push_back(s);
     }
@@ -52,15 +50,10 @@ int calc(string s) {
   int tmp = 0;
   int sign = 1;
   for (int i=0; i<s.size(); i++) {
-    if (s[i] == '+') {
+    if (s[i] == '+' || s[i] == '-') {
       total += sign * tmp;
       tmp = 0;
-      sign = 1;
-    }
-    else if (s[i] == '-') {
-      total += sign * tmp;
-      tmp = 0;
-      sign = -1;
+      sign = s[i] == '+' ? 1 : -1;
     }
     else if (s[i] == ' ') {
       tmp *= 10;
@@ -69,7 +62,7 @@ int calc(string s) {
       tmp += to_int(s[i]);
     }
   }
-
+  // 마지막 남은 숫자 더해주기
   total += sign * tmp;
 
   return total;
