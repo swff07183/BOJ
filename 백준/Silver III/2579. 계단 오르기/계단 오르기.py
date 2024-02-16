5d1eb724cbaa9f97b681dfb0b75e4b6396dca01b
SeongMok Hong <swff07183@gmail.com>
2024-02-16 21:34:50 +0900

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver III/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver III/2579. 계단 오르기/계단 오르기.py
@@ -1,15 +1,15 @@
 N = int(input())
-arr = [0]+[int(input()) for _ in range(N)]
-dp = [0 for _ in range(N+1)]
 
-try:
-    dp[0] = 0
-    dp[1] = arr[1]
-    dp[2] = arr[1] + arr[2]
-except IndexError:
-    pass
+arr = [0] + [int(input()) for _ in range(N)] + [0, 0, 0]
+dp = [0 for _ in range(N+10)]
+dp[1] = arr[1]
+dp[2] = arr[1] + arr[2]
 
-for i in range(3, N+1):
-    dp[i] = max(dp[i-2]+arr[i], dp[i-3] + arr[i]+arr[i-1])
+for n in range(3, N+1):
+    """
+    n-3, n-1, n
+    n-2, n
+    """
+    dp[n] = max(arr[n] + arr[n-1] + dp[n-3], dp[n-2] + arr[n])
 
 print(dp[N])
